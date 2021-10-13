940f7957ba71391801a2f827e7a581cae2de481e
Chris Kim <ckjr92@gmail.com>
2021-10-13 16:13:16 -0600

    More final changes

--- lab.js
+++ lab.js
@@ -167,14 +167,12 @@ let passingScore = 7
 // // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 
-// let z = 5  
+let z = 5  
 
-// while (z > 0) {
-//   for (let i = 5; i > 0; i--) {
-//     console.log(i);
-//     for (let j = z - 1; j > 0; j--) {
-//       console.log(j)
-//     }
-//     z = z - 1
-//   }
-// }
+
+while (z > 0) {
+  for (let j = z; j > 0; j--) {
+    console.log(j)
+  }
+  z -= 1
+}
